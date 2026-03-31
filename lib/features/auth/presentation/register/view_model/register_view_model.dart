5fa6fde5567d48e0597a0adf082e37583c70e7a0
Mohamed Ehab <mohamedehabelsawy03@gmail.com>
2026-03-31 13:14:13 +0200

    refactor/register-view-model: remove RegisterUseCase dependency
    - Comment out `RegisterUseCase` dependency in `RegisterViewModel`.
    - Update `RegisterViewModel` constructor to remove the required `RegisterUseCase` parameter.
    - Remove unused `RegisterUseCase` import.

--- lib/features/auth/presentation/register/view_model/register_view_model.dart
+++ lib/features/auth/presentation/register/view_model/register_view_model.dart
@@ -1,10 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:super_fitness/features/auth/domain/use_cases/register_use_case.dart';
 
 part 'register_state.dart';
 
 class RegisterViewModel extends Cubit<RegisterState> {
-  final RegisterUseCase _registerUseCase;
+  // final RegisterUseCase _registerUseCase;
 
-  RegisterViewModel(this._registerUseCase) : super(RegisterInitial());
+  RegisterViewModel() : super(RegisterInitial());
 }
