8e4d24df43f8dde8396ee756b15829924f42acf8
Omar Wheed <wheedomar88@gmail.com>
2026-04-29 18:29:33 +0300

    refactor: clean up unused imports and formatting
    - Remove redundant blank line in `ApiConstants`.
    - Remove unused `dart:async` import in `ChatViewModel` tests.

--- test/features/chat_ai/presentation/view_model/chat_view_model_test.dart
+++ test/features/chat_ai/presentation/view_model/chat_view_model_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:super_fitness/core/bloc/base_state.dart';
 import 'package:super_fitness/core/error_handling/result.dart';
